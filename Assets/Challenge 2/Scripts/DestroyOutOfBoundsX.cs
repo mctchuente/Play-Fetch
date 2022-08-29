b5b8be9326f0546c5851a0529eae766d8fc2a5bb
Bertrand <mctchuente@gmail.com>
2022-08-29 17:03:22 +0100

    Project with bonus completed

---
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = 40;
+    private float bottomLimit = 0;
+    public static bool isGameOver = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < -leftLimit)
+        {
+            Destroy(gameObject);
+            isGameOver = true;
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+            isGameOver = false;
+        }
+
+        if (isGameOver)
+        {
+            Debug.LogWarning("Game Over!");
+        }
+    }
+}
---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float startDelay = 3.0f;
+    private float spawnInterval = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnDog", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+    }
+
+    // Spawn dog in play area
+    void SpawnDog ()
+    {
+        // instantiate dog and send
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+}
